231cbeb837d49de57d78da812da3144fda83b04f
tetorgal <hector.galarza.css@gmail.com>
2024-10-20 12:38:48 -0600

    Cambiar configuración de tailwind, agregar fuente personalizada

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,27 @@
+import { nextui } from '@nextui-org/react'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'geist-sans': 'var(--font-geist-sans)',
+        'geist-mono': 'var(--font-geist-mono)'
+      },
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)'
       }
     }
   },
-  plugins: []
+  darkMode: 'class',
+  plugins: [nextui()]
 }
 export default config
