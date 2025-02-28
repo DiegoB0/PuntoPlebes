614cb705b30c45d7cb923bdb9521abaffb20dea5
tetorgal <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>
2025-02-27 20:14:10 -0600

    feat(api): add API key support in axios instance and update .env.template

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -5,9 +5,14 @@ import axios, {
 import Cookies from 'js-cookie'
 
 const apiURL = process.env.NEXT_PUBLIC_API_URL
+const apiKey = process.env.NEXT_PUBLIC_API_KEY
 
 if (apiURL == null) {
   throw new Error('API URL is not defined')
+}
+
+if (apiKey == null) {
+  throw new Error('API KEY is not defined')
 }
 
 const axiosInstance: AxiosInstance = axios.create({
@@ -25,6 +30,19 @@ const setToken = (
   }
   return config
 }
+const setApiKey = (
+  config: InternalAxiosRequestConfig
+): InternalAxiosRequestConfig => {
+  if (config.headers) {
+    config.headers['x-api-key'] = apiKey
+  }
+  return config
+}
+
+axiosInstance.interceptors.request.use(
+  (config) => setApiKey(config),
+  async (error) => await Promise.reject(error)
+)
 
 axiosInstance.interceptors.request.use(
   (config) => setToken(config),
