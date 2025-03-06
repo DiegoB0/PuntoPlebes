5fd4d0b1f5be5cf6661e857cd59ddeebfad40541
tetorgal <hector.galarza.css@gmail.com>
2025-03-06 13:54:07 -0600

    feat(categories): add category management functions including save, update, delete, and active category handling

--- src/store/categories/categorySlice.ts
+++ src/store/categories/categorySlice.ts
@@ -7,6 +7,8 @@ import type { Category, CategorySlice } from '@/types/categories'
 
 export const useCategories: StateCreator<CategorySlice> = (set, get) => ({
   categories: [],
+  activeCategory: null,
+  loading: false,
 
   getCategories: async () => {
     await axiosInstance.get('/category').then(({ data }) => {
@@ -14,6 +16,74 @@ export const useCategories: StateCreator<CategorySlice> = (set, get) => ({
         categories: data
       })
     })
+  },
+
+  setActiveCategory: async (id) => {
+    const category = get().categories.find((category) => category.id === id)
+    set({ activeCategory: category })
+  },
+  saveCategory: async (category) => {
+    try {
+      set({ loading: true })
+      const { data } = await axiosInstance.post('/category', category)
+      set((state) => ({
+        categories: [data.category],
+        loading: false
+      }))
+      toastAlert({ title: 'Categoria agregada con éxito', icon: 'success' })
+      return data
+    } catch (error) {
+      console.error('Error al agregar la categoria:', error)
+      toastAlert({
+        title: 'Error al agregar la categoria',
+        icon: 'error'
+      })
+      set({ loading: false })
+      throw error
+    }
+  },
+
+  updateCategory: async (category, id) => {
+    try {
+      set({ loading: true })
+      const { data } = await axiosInstance.patch(`/category/${id}`, category)
+      set((state) => ({
+        categories: state.categories.map((category) =>
+          category.id === id ? data : category
+        ),
+        loading: false
+      }))
+      toastAlert({ title: 'Categoria actualizada', icon: 'success' })
+      return data
+    } catch (error) {
+      console.error('Error al actualizar la categoria:', error)
+      toastAlert({
+        title: 'Error al actualizar la categoria',
+        icon: 'error'
+      })
+      set({ loading: false })
+      throw error
+    }
+  },
+
+  deleteCategory: async (id) => {
+    try {
+      set({ loading: true })
+      await axiosInstance.delete(`/category/${id}`)
+      set((state) => ({
+        categories: state.categories.filter((category) => category.id !== id),
+        loading: false
+      }))
+      toastAlert({ title: 'Categoria eliminada', icon: 'success' })
+    } catch (error) {
+      console.error('Error al eliminar la categoria:', error)
+      toastAlert({
+        title: 'Error al eliminar la categoria',
+        icon: 'error'
+      })
+      set({ loading: false })
+      throw error
+    }
   }
 })
 
--- src/types/categories.ts
+++ src/types/categories.ts
@@ -1,6 +1,12 @@
 export interface CategorySlice {
   categories: Category[]
+  activeCategory: Category | null
+  loading: boolean
   getCategories: () => Promise<void>
+  saveCategory: (category: CategoryInputs) => Promise<void>
+  updateCategory: (category: CategoryInputs, id: number) => Promise<void>
+  deleteCategory: (id: number) => void
+  setActiveCategory: (id: number) => void
 }
 
 export interface Category {
@@ -12,6 +18,10 @@ export interface Category {
 export interface CategoryTableProps {
   id: number
   created_at: string
+  category_name: string
+  menu_type: string
+}
+export interface CategoryInputs {
   category_name: string
   menu_type: string
 }
