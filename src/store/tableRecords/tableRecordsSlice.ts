a030a901f7a1f0c3eb5c1df910ec2753beaa1725
tetorgal <hector.galarza.css@gmail.com>
2025-03-06 13:54:43 -0600

    refactor(tableRecords): comment out console.log statements in tableRecordsSlice

--- src/store/tableRecords/tableRecordsSlice.ts
+++ src/store/tableRecords/tableRecordsSlice.ts
@@ -10,7 +10,7 @@ export const useSelectedRecordsStore: StateCreator<tableRecordsSlice> = (
   // Actualiza el ID para editar un solo registro
   setSingleId: (id) => {
     set({ singleId: id })
-    console.log(id)
+    // console.log(id)
   },
 
   // Agrega un ID a la lista de IDs seleccionados (para eliminación o edición múltiple)
@@ -18,7 +18,7 @@ export const useSelectedRecordsStore: StateCreator<tableRecordsSlice> = (
     set((state) => ({
       multipleIds: [...state.multipleIds, id]
     }))
-    console.log(id)
+    // console.log(id)
   },
 
   // Remueve un ID de la lista de seleccionados
@@ -26,14 +26,14 @@ export const useSelectedRecordsStore: StateCreator<tableRecordsSlice> = (
     set((state) => ({
       multipleIds: state.multipleIds.filter((selectedId) => selectedId !== id)
     }))
-    console.log(id)
+    // console.log(id)
   },
 
   // Limpia todos los IDs seleccionados, útil después de ejecutar una acción
   clearSelectedIds: () => {
     set({ multipleIds: [] })
-    console.log('clearSelectedIds')
   }
+  // console.log('clearSelectedId<')
 })
 
 export const useSelectedRecords = create<tableRecordsSlice>()((...a) => ({
